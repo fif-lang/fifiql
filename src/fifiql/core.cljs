690056912814be5f09c2ec117f43bd00d465282c
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-03-31 22:54:11 -0400

    started work on ui

--- src/fifiql/core.cljs
+++ src/fifiql/core.cljs
@@ -1,5 +1,10 @@
-(ns fifiql.core)
+(ns fifiql.core
+  (:require
+   [reagent.core :as r]
+   [fifiql.ui.layout :refer [base-layout]]))
+
 
 (enable-console-print!)
 
-(println "Hello World!")
+
+(r/render [base-layout] (.querySelector js/document "#app"))
---
+++ src/fifiql/ui/layout.cljs
@@ -0,0 +1,8 @@
+(ns fifiql.ui.layout)
+
+
+(defn base-layout []
+  [:<>
+   [:div.header "header"]
+   [:div.sidebar "sidebar"]
+   [:div.editor "editor"]])
